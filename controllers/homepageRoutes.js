04e0b9bc8577518c3403a63bd7669c5bf5b32836
Bonee94 <bonee94@yahoo.com>
2023-01-31 10:36:42 -0600

    Added homepage rendering with session var. Added routing to login page.

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,3 +1,4 @@
+const { response } = require("../../Taylors-Note-Taker/db");
 const { Post } = require("../models");
 
 const router = require("express").Router();
@@ -6,9 +7,20 @@ router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll();
 
-    const posts = postData.map((post) => post.get({ plain:true}))
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.status(200).render("homepage", { posts });
+    res.status(200).render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err); 
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    res.status(200).render("login");
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,6 @@ const apiRoutes = require('./api');
 const homepageRoutes = require('./homepageRoutes');
 
 router.use('/', homepageRoutes);
-//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
