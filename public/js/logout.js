e8c4e5479ae73d49bcad75c0246933af7326604b
Bonee94 <bonee94@yahoo.com>
2023-01-31 10:33:02 -0600

    Created logout function to call user api logout route

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,7 +1,14 @@
-const logoutEvent = (event) => {
-    event.preventDefault();
-  
-    alert("Logged out");
-  };
-  
-  document.getElementById("logout-btn").addEventListener("click", logoutEvent);
+const logoutEvent = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.getElementById('logout-btn').addEventListener('click', logoutEvent);
