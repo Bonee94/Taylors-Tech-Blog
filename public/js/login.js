20bc21588b11f2c6c8bdcf8d317719dc6c09cec3
Bonee94 <bonee94@yahoo.com>
2023-01-31 10:28:46 -0600

    Added registration function to login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,30 @@
-const loginEvent = (event) => {
+const registerEvent = async (event) => {
   event.preventDefault();
 
-  alert("Logged in");
+  const first_name = document
+    .getElementById("first-name-register-user")
+    .value.trim();
+  const last_name = document
+    .getElementById("last-name-register-user")
+    .value.trim();
+  const email = document.getElementById("email-register-user").value.trim();
+  const password = document
+    .getElementById("password-register-user")
+    .value.trim();
+
+  if (first_name && last_name && email && password) {
+    const response = await fetch("/api/users/register", {
+      method: "POST",
+      body: JSON.stringify({ first_name, last_name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.href = '/'
+    }
+  }
 };
 
-document.getElementById("login-btn").addEventListener("click", loginEvent);
+document
+  .getElementById("register-user-btn")
+  .addEventListener("click", registerEvent);
