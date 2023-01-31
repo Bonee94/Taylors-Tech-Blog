da2ec90ba55cc14c9f1325787892d9ef20dd7717
Bonee94 <bonee94@yahoo.com>
2023-01-31 14:06:09 -0600

    Added username to registration and login and api login route.

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -3,7 +3,7 @@ const { Post, User } = require("../../models");
 
 router.post("/login", async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
       res
--- public/js/register.js
+++ public/js/register.js
@@ -8,6 +8,7 @@ const registerEvent = async (event) => {
       .getElementById("last-name-register-user")
       .value.trim();
     const email = document.getElementById("email-register-user").value.trim();
+    const username = document.getElementById("username-register-user").value.trim();
     const password = document
       .getElementById("password-register-user")
       .value.trim();
@@ -15,7 +16,7 @@ const registerEvent = async (event) => {
     if (first_name && last_name && email && password) {
       const response = await fetch("/api/users/register", {
         method: "POST",
-        body: JSON.stringify({ first_name, last_name, email, password }),
+        body: JSON.stringify({ first_name, last_name, email, username, password }),
         headers: { "Content-Type": "application/json" },
       });
   
