f786a8127c8404c2e1721ad49e9059fe5aa52520
Bonee94 <bonee94@yahoo.com>
2023-02-21 03:00:10 -0600

    Updated api index for routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
 const users = require("./users");
+const comments = require("./comments");
+const posts = require("./posts");
 
 router.use("/users", users);
+router.use("/posts", posts);
+router.use("/comments", comments);
 
 module.exports = router;
