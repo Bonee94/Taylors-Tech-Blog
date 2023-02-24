3ab5bb22271b5b210c20dce26e1b88c5e095de30
Bonee94 <bonee94@yahoo.com>
2023-02-23 23:48:20 -0600

    added event listeners to posts on dashboard page to select a single post on click

---
+++ public/js/userSinglePost.js
@@ -0,0 +1,8 @@
+//collects all post btn elements and creates event listener for each post to call redirect
+document.querySelectorAll(".post-select-btn").forEach((btn) => {
+    const postId = btn.getAttribute("name");
+  
+    btn.addEventListener("click", () => {
+      document.location = `/dashboard/post/${postId}`;
+    });
+  });
