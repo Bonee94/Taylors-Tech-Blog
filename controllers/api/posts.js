07455d72e60ca36cb6c5b3b67a5ff81c4b12ba35
Bonee94 <bonee94@yahoo.com>
2023-02-25 12:26:21 -0600

    Created api delete post route

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -7,50 +7,61 @@ router.post("/", async (req, res) => {
     const user = await User.findByPk(req.session.user_id);
 
     const post = {
-        author: user.username,
-        title: req.body.title,
-        content: req.body.content,
-        date_created: Date.now(),
-        user_id: req.session.user_id,
-    }
+      author: user.username,
+      title: req.body.title,
+      content: req.body.content,
+      date_created: Date.now(),
+      user_id: req.session.user_id,
+    };
 
     await Post.create(post);
 
-    res.status(200).json('Post Saved')
+    res.status(200).json("Post Saved");
   } catch (error) {
     res.status(400).json(error);
   }
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const postData = await Post.findByPk(req.session.viewing_post_id)
+    const postData = await Post.findByPk(req.session.viewing_post_id);
 
     const post = await postData.get({ plain: true });
 
-    console.log(post)
-
-    res.status(200).json(post)
+    res.status(200).json(post);
   } catch (error) {
     res.status(400).json(error);
-
   }
 });
 
 //updates post in db
 router.put("/", async (req, res) => {
   try {
+    console.log('Put route: ', req.session.viewing_post_id)
 
     const post = {
-        title: req.body.title,
-        content: req.body.content,
-    }
+      title: req.body.title,
+      content: req.body.content,
+    };
 
     await Post.update(post, {
-      where: {id: req.session.viewing_post_id}
+      where: { id: req.session.viewing_post_id },
     });
 
-    res.status(200).json('Post Updated')
+    res.status(200).json("Post Updated");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    console.log('Delete route: ', req.session.viewing_post_id)
+    await Post.destroy({
+      where: { id: req.session.viewing_post_id },
+    });
+
+    res.status(200).json("Post Updated");
   } catch (error) {
     res.status(400).json(error);
   }
