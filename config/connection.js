e74c78c1664b449d076bc4e1fd5f660b3ce48df9
Bonee94 <bonee94@yahoo.com>
2023-02-25 21:09:40 -0600

    debugging heroku variables

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // create connection to our db
 const sequelize = process.env.NODE_ENV == 'production'
-  ? new Sequelize(process.env.JAWSDB_URL)
+  ? new Sequelize(process.env.JAWSDB_URL, {dialect: 'mysql'})
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
