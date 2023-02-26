0e1062dc585c381544eb0ca805f4db77977b3c56
Bonee94 <bonee94@yahoo.com>
2023-02-26 03:09:06 -0600

    testing heroku

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -24,6 +24,7 @@ router.post("/login", async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.viewing_post_id = 0;
 
       res.json({ message: "You are now logged in!" });
     });
