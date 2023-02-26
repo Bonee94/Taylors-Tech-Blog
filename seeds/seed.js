15a126eee1a26e470f4232d9a71ce8c154b501e0
Bonee94 <bonee94@yahoo.com>
2023-02-25 20:01:04 -0600

    Added and updated seed files for testing

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,23 @@
-const sequelize = require('../config/connection');
-const {Post} = require('../models');
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
 
-const postData = require('./postSeedData.json')
-
-console.log(postData)
+const postData = require("./postSeedData.json");
+const userData = require("./userSeedData.json");
+const commentData = require("./commentSeedData.json");
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData);
   
-    await Post.bulkCreate(postData);
-  
-    process.exit(0);
-  };
-  
+  await Comment.bulkCreate(commentData);
+
+  process.exit(0);
+};
+
 seedDatabase();
