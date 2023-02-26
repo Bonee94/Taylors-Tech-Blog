adafc3224118d5f41d959b27b3497b99187cc4a7
Bonee94 <bonee94@yahoo.com>
2023-02-26 01:15:19 -0600

    test

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -103,7 +103,7 @@ router.get("/dashboard/post/:id", withAuth, async (req, res) => {
     }
 
     req.session.save(() => {
-      viewing_post_id = req.params.id;
+      req.session.viewing_post_id = req.params.id;
     });
 
     res.status(200).render("dashboardSinglePost", {
@@ -135,7 +135,7 @@ router.get("/post/:id", async (req, res) => {
     }
 
     req.session.save(() => {
-      viewing_post_id = req.params.id;
+      req.session.viewing_post_id = req.params.id;
     });
 
     res.status(200).render("singlePost", {
